95cf93957a620fc1b48b2fe2ecf6fbee30df2584
Kentaro <kenchik2004@gmail.com>
2025-03-21 13:29:16 +0900

    MemoryLeak例外を追加

--- src/System/Exception.cpp
+++ src/System/Exception.cpp
@@ -58,3 +58,16 @@ OutOfRangeException::OutOfRangeException(int index, int array_size, const char* 
 	message += std::to_string(array_size);
 	message += "] を超えています。\n";
 }
+
+MemoryLeakException::MemoryLeakException(const char* integer_name, const char* file_name, int line, const char* func_name)
+{
+	message = "ファイル名: ";
+	message += file_name;
+	message += "\n行: ";
+	message += std::to_string(line);
+	message += "\n関数名: ";
+	message += func_name;
+	message += "\nで例外スロー: \n";
+	message += integer_name;
+	message += "が解放できませんでした。循環参照が起こっている可能性があります。\n 変数をstd::weak_ptrで保持、メモリ解放時にstd::shared_ptrにnullptrを代入などを試してみてください。";
+}
