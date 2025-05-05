d86329bde3b417e032840f257eedc6ff3c2da6a4
Kentaro <kenchik2004@gmail.com>
2025-05-06 08:06:05 +0900

    Nullセーフのshared,weak,uniqueの実装
    ・いちいちぬるぽでクラッシュしちゃたまんないので、nullptrアクセスにより例外を投げるクラスを作成

--- src/System/typedef.cpp
+++ src/System/typedef.cpp
@@ -25,3 +25,20 @@ std::string WStr2Str(std::wstring in)
 	WideCharToMultiByte(CP_OEMCP, 0, in.c_str(), static_cast<int>(in.size()), &out[0], size_needed, NULL, NULL);
 	return out;
 }
+
+const char* TypeInfo::ClassName() const
+{
+	return class_name.c_str();
+}
+
+size_t TypeInfo::ClassSize() const
+{
+	return class_size;
+}
+
+const TypeInfo* TypeInfo::Parent() const
+{
+	return parent;
+}
+
+TypeInfo TypeInfo::Root = TypeInfo("root", sizeof(TypeInfo), nullptr);
